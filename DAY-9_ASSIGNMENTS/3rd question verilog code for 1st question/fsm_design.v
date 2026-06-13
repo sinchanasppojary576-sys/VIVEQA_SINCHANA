4787056eee3e59a3578486cc12e5028d738dfe1b
Sinchana <sinchanasppojary576@gmail.com>
2026-06-13 20:08:33 +0530

    Create fsm_design.v

---
+++ DAY-9_ASSIGNMENTS/3rd question verilog code for 1st question/fsm_design.v
@@ -0,0 +1,69 @@
+module fsm(
+    input clk,
+    input rst,
+    input din,
+    output reg out
+);
+
+parameter S0 = 2'b00,
+          S1 = 2'b01,
+          S2 = 2'b10,
+          S3 = 2'b11;
+
+reg [1:0] state, next_state;
+
+// State Register
+always @(posedge clk) begin
+    if(rst)
+        state <= S0;
+    else
+        state <= next_state;
+end
+
+// Output Toggle Logic
+always @(posedge clk) begin
+    if(rst)
+        out <= 1'b0;
+    else if(state == S2 && din == 1'b0)   // 110 detected
+        out <= ~out;
+end
+
+// Next State Logic
+always @(*) begin
+    case(state)
+
+        S0: begin
+            if(din)
+                next_state = S1;
+            else
+                next_state = S0;
+        end
+
+        S1: begin
+            if(din)
+                next_state = S2;
+            else
+                next_state = S0;
+        end
+
+        S2: begin
+            if(din)
+                next_state = S2;
+            else
+                next_state = S3;
+        end
+
+        S3: begin
+            if(din)
+                next_state = S1;
+            else
+                next_state = S0;
+        end
+
+        default:
+            next_state = S0;
+
+    endcase
+end
+
+endmodule
