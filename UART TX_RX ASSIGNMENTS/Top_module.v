453c2312ca9416d68ff5b32e698f688a44818492
Sinchana <sinchanasppojary576@gmail.com>
2026-06-28 20:37:50 +0530

    Update Top_module.v

--- UART TX_RX ASSIGNMENTS/Top_module.v
+++ UART TX_RX ASSIGNMENTS/Top_module.v
@@ -1,91 +1,35 @@
-`timescale 1ns/1ps
+module uart_loopback_top(
 
-module tb_uart_loopback_top;
+    input clk,
+    input rst,
 
-reg clk;
-reg rst;
+    input tx_start,
+    input [7:0] tx_data,
 
-reg tx_start;
-reg [7:0] tx_data;
+    output tx_busy,
 
-wire tx_busy;
-wire [7:0] rx_data;
-wire rx_done;
-
-uart_loopback_top DUT (
-
-    .clk(clk),
-    .rst(rst),
-
-    .tx_start(tx_start),
-    .tx_data(tx_data),
-
-    .tx_busy(tx_busy),
-
-    .rx_data(rx_data),
-    .rx_done(rx_done)
+    output [7:0] rx_data,
+    output rx_done
 
 );
 
-always #20 clk = ~clk;
+wire uart_wire;
 
-task send_data;
-input [7:0] data;
-begin
+uart_tx TX (
+    .clk(clk),
+    .rst(rst),
+    .tx_start(tx_start),
+    .tx_data(tx_data),
+    .tx(uart_wire),
+    .tx_busy(tx_busy)
+);
 
-    @(posedge clk);
-
-    tx_data  <= data;
-    tx_start <= 1'b1;
-
-    @(posedge clk);
-
-    tx_start <= 1'b0;
-
-    wait(rx_done);
-
-    if(rx_data == data)
-        $display("PASS : Sent=%h Received=%h Time=%t",
-                 data, rx_data, $time);
-    else
-        $display("FAIL : Sent=%h Received=%h Time=%t",
-                 data, rx_data, $time);
-
-    #100000;
-
-end
-endtask
-
-initial
-begin
-
-    clk = 0;
-    rst = 1;
-    tx_start = 0;
-    tx_data = 0;
-
-    #500;
-
-    rst = 0;
-
-    send_data(8'h55);
-
-    send_data(8'hAA);
-
-    send_data(8'hF0);
-
-    send_data(8'h0F);
-
-    send_data(8'hA5);
-
-    send_data(8'h5A);
-
-    #1000000;
-
-    $display("TEST COMPLETED");
-
-    $stop;
-
-end
+uart_rx RX (
+    .clk(clk),
+    .rst(rst),
+    .rx(uart_wire),
+    .rx_data(rx_data),
+    .rx_done(rx_done)
+);
 
 endmodule
